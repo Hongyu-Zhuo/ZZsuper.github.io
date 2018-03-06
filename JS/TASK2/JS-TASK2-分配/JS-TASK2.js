63a708bb1014c80923b853ef9cb524a2524b1ea3
ZZsuper <hongyuzhuo@gmail.com>
2018-03-06 23:11:41 +0800

    0

--- JS/TASK2/JS-TASK2-分配/JS-TASK2.js
+++ JS/TASK2/JS-TASK2-分配/JS-TASK2.js
@@ -3,5 +3,5 @@ console.log("JS是否存在逻辑问题、变量问题、参数问题等等")
 console.log("JS符号是否存在问题")
 
 function toSimple () {
-	window.location.href="www.baidu.com";
+	window.location.href="https://ZZsuper.github.io/JS/TASK2/JS-TASK2-分配/JS-TASK2-分配.html";
 }
